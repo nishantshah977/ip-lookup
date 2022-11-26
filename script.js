f69d901a4ced4b71515889ea62d4d0efda991398
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2022-11-26 12:10:55 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function start(){
+document.querySelector('#img').style.display='block';
   let ip = document.querySelector("input").value;
   // Fetching Api
   
   fetch(`http://ip-api.com/json/${ip}`)
   .then((response) => {
-      return response.json();
+      return response.json();  
   })
   .then((data) => {
       const {query} = data;
-      const {status} = data;
+      let {status} = data;
       const {message} = data;
       const {country} = data;
       const {regionName} = data;
@@ -17,18 +18,18 @@ function start(){
       const {city} = data;
       const {isp} = data;
       const {org} = data;
-      if(message != undefined){
       alert(message);
-      }
-      // Setting Value
        document.querySelector("#status").innerHTML=status;
 document.querySelector("#country").innerHTML=country;       
 document.querySelector("#state").innerHTML=regionName;  
 document.querySelector("#latitude").innerHTML=lat;
 document.querySelector("#longitude").innerHTML=lon;
-document.querySelector("#city").innerHTML=city; 
-document.querySelector("#isp").innerHTML=isp;   
-document.querySelector("#organization").innerHTML=org;      
+let cit = document.querySelector("#city");
+cit.innerHTML=city; 
+document.querySelector("#isp").innerHTML=isp;   document.querySelector("#organization").innerHTML=org;  
+
+if(cit.innerHTML != null){
+    document.querySelector('#img').style.display="none";
+ }  document.querySelector("#display").style.display="block";   
   });
-  document.querySelector("#display").style.display="block";
 }
